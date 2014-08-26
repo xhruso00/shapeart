48911244ede8addd1dae8bfbf6fbd86ed3e497cb
Marek Hrusovsky <xhruso00@gmail.com>
2014-08-26 14:57:14 +0200

    Initial Commit

---
+++ ShapeArt/SADocument.h
@@ -0,0 +1,13 @@
+//
+//  SADocument.h
+//  ShapeArt
+//
+//  Created by Marek Hrusovsky on 26/08/14.
+//  Copyright (c) 2014 Marek Hrusovsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SADocument : NSDocument
+
+@end
